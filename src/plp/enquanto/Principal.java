c4387cc85806018d80e3ed50f6a2ba66cf94753e
Leonardo Lucena <leonardo.lucena@escolar.ifrn.edu.br>
2018-03-20 16:22:03 -0300

    Atualizando para ANTLR 4.7.1

--- src/plp/enquanto/Principal.java
+++ src/plp/enquanto/Principal.java
@@ -2,21 +2,26 @@ package plp.enquanto;
 
 import java.io.IOException;
 
-import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
+import plp.enquanto.linguagem.Linguagem.Programa;
 import plp.enquanto.parser.EnquantoLexer;
 import plp.enquanto.parser.EnquantoParser;
 import plp.enquanto.parser.MeuListener;
-import static plp.enquanto.linguagem.Linguagem.*;
-import static java.util.Arrays.*;
 
 public class Principal {
 
-	private static ParseTree parse(String programa) {
-		final ANTLRInputStream input = new ANTLRInputStream(programa);
+	private static ParseTree parse(String arq) {
+		CharStream input;
+		try {
+			input = CharStreams.fromFileName(arq);
+		} catch (IOException e) {
+			input = CharStreams.fromString("exiba \"Arquivo não encontrado.\"");
+		}
 		final EnquantoLexer lexer = new EnquantoLexer(input);
 		final CommonTokenStream tokens = new CommonTokenStream(lexer);
 		final EnquantoParser parser = new EnquantoParser(tokens);
@@ -24,23 +29,12 @@ public class Principal {
 	}
 
 	public static void main(String... args) throws IOException {
-		String programa = "x:=10; y:=leia ; c:= x + y; "
-				+ "se 30<=c entao escreva c senao exiba \"menor\"";
-		final ParseTree tree = parse(programa);
+		String arq = args[0];
+		final ParseTree tree = parse(arq);
 		final ParseTreeWalker walker = new ParseTreeWalker();
 		final MeuListener listener = new MeuListener();
 		walker.walk(listener, tree);
 		Programa p1 = listener.getPrograma();
-		// O parser devolve um objeto 'Programa' semelhante ao programa a seguir:
-		Programa p2 = new Programa(asList(
-				new Atribuicao("x", new Inteiro(10)),                       // x := 10
-				new Atribuicao("y", leia),                                  // y := leia
-				new Atribuicao("c", new ExpSoma(new Id("x"), new Id("y"))), // c := x + y
-				new Se(new ExpMenorIgual(new Inteiro(30), new Id("c")),     // se 30 <= c entao
-						new Escreva(new Id("c")),                           // escreva c
-						new Exiba("menor"))                                 // senao exiba "menor"
-				));
 		p1.execute();
-		p2.execute();
 	}
 }
